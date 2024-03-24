5120342744e9b071cf2240a6adedfc28d8734b80
morgan.9 <morgan.9@kakaocorp.com>
2024-03-24 15:54:43 +0900

    refactor: 메시지 ViewMessage로 별도 분리
    (cherry picked from commit 5cb6cef946a3c2c8a4ed050c0766adbeaa3bb4af)

--- src/main/java/NumberBaseball/view/NumberBaseballView.java
+++ src/main/java/NumberBaseball/view/NumberBaseballView.java
@@ -3,28 +3,32 @@ package NumberBaseball.view;
 import java.util.Scanner;
 
 public class NumberBaseballView {
+
     public String inputNumber() {
+        System.out.print(ViewMessage.INPUT_NUMBER);
+        return getInput();
+    }
+
+    private static String getInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("숫자를 입력해주세요 : ");
         return scanner.nextLine();
     }
 
     public void displayResult(int strike, int ball) {
         if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
+            System.out.println(ViewMessage.NOTHING);
             return;
         }
 
         String result = "";
-        if (strike > 0) result += strike + "스트라이크 ";
-        if (ball > 0) result += ball + "볼";
+        if (strike > 0) result += strike + ViewMessage.STRIKE + " ";
+        if (ball > 0) result += ball + ViewMessage.BALL;
         System.out.println(result);
     }
 
     public String displayRestart() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        System.out.println(ViewMessage.GAME_END);
+        System.out.println(ViewMessage.RESTART);
+        return getInput();
     }
 }
