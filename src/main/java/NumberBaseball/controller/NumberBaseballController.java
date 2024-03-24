866709b7ccef5fdab94f3b8747b424a57ac82b4f
morgan.9 <morgan.9@kakaocorp.com>
2024-03-24 17:35:32 +0900

    Revert "refactor: NumberBaseballController 재시작 로직 model로 책임 변경"
    This reverts commit d9b6b8f6f120fdb366559ab4865aedd62b4bd805.

--- src/main/java/NumberBaseball/controller/NumberBaseballController.java
+++ src/main/java/NumberBaseball/controller/NumberBaseballController.java
@@ -16,34 +16,49 @@ public class NumberBaseballController {
     }
 
     public void startGame() {
-        while (model.isGameContinuing()) {
-            playRound();
+        boolean continueGame = true;
+        while (continueGame) {
+            continueGame = playRound();
         }
     }
 
-    private void playRound() {
+    private boolean playRound() {
         try {
             String input = view.displayRound();
             InputValidator.isValid(input);
             BaseBallCount baseBallCount = model.checkCount(List.of(input.split("")));
             view.displayResult(baseBallCount.getStrike(), baseBallCount.getBall());
-            if (baseBallCount.getStrike() == model.getNumberLength()) {
-                model.setGameContinuing(promptRestartGame());
-            }
+            return checkGameContinue(baseBallCount);
         } catch (Exception e) {
             view.displayErrorMessage(e.getMessage());
+            return true;
         }
+    }
+
+    private boolean checkGameContinue(BaseBallCount baseBallCount) {
+        if (baseBallCount.getStrike() == model.getNumberLength()) {
+            return promptRestartGame();
+        }
+        return true;
     }
 
     private boolean promptRestartGame() {
         while (true) {
             try {
-                String restartOption = view.displayRestart();
-                InputValidator.isRestartValid(restartOption);
-                return model.processRestartOption(restartOption);
+                return !checkRestartGame();
             } catch (Exception e) {
                 view.displayErrorMessage(e.getMessage());
             }
         }
+    }
+
+    private boolean checkRestartGame() {
+        String restartOption = view.displayRestart();
+        InputValidator.isRestartValid(restartOption);
+        if ("2".equals(restartOption)) {
+            return true;
+        }
+        model.reset();
+        return false;
     }
 }
