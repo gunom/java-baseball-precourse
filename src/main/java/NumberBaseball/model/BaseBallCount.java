2d2b009afc3ef5656adf6e1af42c15f8c77c64e5
morgan.9 <morgan.9@kakaocorp.com>
2024-03-24 15:42:52 +0900

    feat: field final로 수정

--- src/main/java/NumberBaseball/model/BaseBallCount.java
+++ src/main/java/NumberBaseball/model/BaseBallCount.java
@@ -1,8 +1,8 @@
 package NumberBaseball.model;
 
 public class BaseBallCount {
-    private int strike;
-    private int ball;
+    private final int strike;
+    private final int ball;
 
     public BaseBallCount(int strike, int ball) {
         this.strike = strike;
