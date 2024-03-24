50c2df0b0e2ca10ffb388fac064876e9a2f419fb
morgan.9 <morgan.9@kakaocorp.com>
2024-03-24 17:30:42 +0900

    refactor: NumberBaseballModel Random 의존성 추가 반영

--- src/main/java/NumberBaseball/NumberBaseball.java
+++ src/main/java/NumberBaseball/NumberBaseball.java
@@ -4,9 +4,11 @@ import NumberBaseball.controller.NumberBaseballController;
 import NumberBaseball.model.NumberBaseballModel;
 import NumberBaseball.view.NumberBaseballView;
 
+import java.util.Random;
+
 public class NumberBaseball {
     public static void main(String[] args) {
-        NumberBaseballModel model = new NumberBaseballModel();
+        NumberBaseballModel model = new NumberBaseballModel(new Random());
         NumberBaseballView view = new NumberBaseballView();
 
         NumberBaseballController numberBaseballController = new NumberBaseballController(model, view);
