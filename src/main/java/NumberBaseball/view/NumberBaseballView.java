4ea72d85623851d5abbc81ff90c01de14b7b9b2b
morgan.9 <morgan.9@kakaocorp.com>
2024-03-24 17:35:33 +0900

    Revert "feat: NumberBaseballView 판정 결과 출력 기능 추가"
    This reverts commit 22f7c02ce249f456902b30af27df4278d1e37832.

--- src/main/java/NumberBaseball/view/NumberBaseballView.java
+++ src/main/java/NumberBaseball/view/NumberBaseballView.java
@@ -8,21 +8,4 @@ public class NumberBaseballView {
         System.out.print("숫자를 입력해주세요 : ");
         return scanner.nextLine();
     }
-
-    public void displayResult(int strike, int ball) {
-        if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-            return;
-        }
-
-        String result = "";
-        if (strike > 0) {
-            result += strike + "스트라이크 ";
-        }
-
-        if (ball > 0) {
-            result += ball + "볼";
-        }
-        System.out.println(result);
-    }
 }
